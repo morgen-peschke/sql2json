adf88e48c7b9a43c838170eeef301b940ab6a9e0
Morgen Peschke <morgen.peschke@gmail.com>
2019-12-02 08:38:49 -0800

    Add missing functor tests for right-biased either

--- src/test/scala/sql2json/cat/EitherInstancesTest.scala
+++ src/test/scala/sql2json/cat/EitherInstancesTest.scala
@@ -4,5 +4,5 @@ package cat
 import testing.laws.{BifunctorLaws, FunctorLaws}
 
 class EitherBifunctorLaws extends BifunctorLaws[Either, Int, String, Long, String, Long, Int]
-
+class EitherRightFunctorLaws extends FunctorLaws[Bifunctor.RightBiased[Either][String], Int, String, Long]
 class EitherLeftFunctorLaws extends FunctorLaws[Bifunctor.LeftBiased[Either][String], Int, String, Long]
